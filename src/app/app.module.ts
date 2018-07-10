b944c781cf2d7cd49024998357676ac7e43f11ad
carom <cme8625@students.fhv.at>
2018-07-10 19:01:19 +0200

    spelling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { InstructionComponent } from './instruction/instruction.component';
+import { InstructionComponent } from '../components/instruction/instruction.component';
 import { InstructionService } from '../services/instruction-service'
 import { MatStepperModule, MatFormFieldModule, MatInputModule, MatExpansionModule, MatCardModule, MatGridListModule, MatIconModule, MatSnackBarModule, MatListModule, MatMenuModule, MatToolbarModule, MatSelectModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,19 +12,19 @@ import { HttpModule } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/Rx';
 import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
-import { ResourceComponent } from '../app/resource/resource.component';
-import { VideoComponent } from './video/video.component';
-import { AudioComponent } from './audio/audio.component';
-import { LoginComponent } from './login/login.component';
+import { ResourceComponent } from '../components/resource/resource.component';
+import { VideoComponent } from '../components/video/video.component';
+import { AudioComponent } from '../components/audio/audio.component';
+import { LoginComponent } from '../components/login/login.component';
 import { RouterModule, Routes } from '@angular/router';
 import { UserService } from '../services/user-service';
-import { InstructionCreateComponent } from './instruction-create/instruction-create.component';
-import { InstructionStepCreateComponent } from './instruction-step-create/instruction-step-create.component';
-import { InstructionChildStepCreateComponent } from './instruction-child-step-create/instruction-child-step-create.component';
-import { FormUploadComponent } from './form-upload/form-upload.component';
-import { PreviewComponent } from './preview/preview.component';
-import { MenuComponent } from './menu/menu.component';
-import { UserInfoComponent } from './user-info/user-info.component';
+import { InstructionCreateComponent } from '../components/instruction-create/instruction-create.component';
+import { InstructionStepCreateComponent } from '../components/instruction-step-create/instruction-step-create.component';
+import { InstructionChildStepCreateComponent } from '../components/instruction-child-step-create/instruction-child-step-create.component';
+import { FormUploadComponent } from '../components/form-upload/form-upload.component';
+import { PreviewComponent } from '../components/preview/preview.component';
+import { MenuComponent } from '../components/menu/menu.component';
+import { UserInfoComponent } from '../components/user-info/user-info.component';
 import {MatTooltipModule} from '@angular/material/tooltip';
 
 const appRoutes: Routes = [
